672a64fd21a30e949b2623a854fce01af4cd380d
IcyHavit <128344897+IcyHavit@users.noreply.github.com>
2023-05-05 01:41:34 -0600

    Delete scriptFibonacci.sh

--- scriptFibonacci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-gcc fibonacci.c recursos/tiempo.c -lpthread -o fibonacci
-
-./fibonacci 4 10000000 < numeros.txt > salFibonacci.txt
-./fibonacci 4 20000000 < numeros.txt >> salFibonacci.txt
-./fibonacci 4 30000000 < numeros.txt >> salFibonacci.txt
-./fibonacci 4 40000000 < numeros.txt >> salFibonacci.txt
-./fibonacci 4 50000000 < numeros.txt >> salFibonacci.txt
-./fibonacci 4 100000000 < numeros.txt >> salFibonacci.txt
