d4ba9a3d075a9b5888f23e84bf3e2ff54edb6786
IcyHavit <128344897+IcyHavit@users.noreply.github.com>
2023-05-05 20:38:25 -0600

    Update scriptFibonacci1.sh

--- scriptFibonacci1.sh
+++ scriptFibonacci1.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+
+#COMPILAR EL DE HILOS
 gcc fibonacci_conHilos.c recursos/tiempo.c -lpthread -o fibonacci_conHilos
+#COMPILAR EL DE SIN HILOS
 gcc fibonacci_sinHilos.c recursos/tiempo.c -o fibonacci_sinHilos
 
+# Parámetros a pasar al programa DE HILOS
+n="$1"
+output_file="salFibonacci_conHilos.txt"
 
-./fibonacci_conHilos 4 1000000 < numeros.txt >> salFibonacci_conHilos.txt
-./fibonacci_conHilos 4 2000000 < numeros.txt >> salFibonacci_conHilos.txt
-./fibonacci_conHilos 4 3000000 < numeros.txt >> salFibonacci_conHilos.txt
-./fibonacci_conHilos 4 4000000 < numeros.txt >> salFibonacci_conHilos.txt
-./fibonacci_conHilos 4 5000000 < numeros.txt >> salFibonacci_conHilos.txt
-./fibonacci_conHilos 4 10000000 < numeros.txt >> salFibonacci_conHilos.txt
+./fibonacci_conHilos "$n" 10000000 <numeros.txt > "$output_file"
+./fibonacci_conHilos "$n" 20000000 <numeros.txt >> "$output_file"
+./fibonacci_conHilos "$n" 30000000 <numeros.txt >> "$output_file"
+./fibonacci_conHilos "$n" 40000000 <numeros.txt >> "$output_file"
+./fibonacci_conHilos "$n" 50000000 <numeros.txt >> "$output_file"
+./fibonacci_conHilos "$n" 100000000 <numeros.txt >> "$output_file"
 
 
-./fibonacci_sinHilos 1000000 < numeros.txt > salFibonacci_sinHilos.txt
-./fibonacci_sinHilos 2000000 < numeros.txt >> salFibonacci_sinHilos.txt
-./fibonacci_sinHilos 3000000 < numeros.txt >> salFibonacci_sinHilos.txt
-./fibonacci_sinHilos 4000000 < numeros.txt >> salFibonacci_sinHilos.txt
-./fibonacci_sinHilos 5000000 < numeros.txt >> salFibonacci_sinHilos.txt
-./fibonacci_sinHilos 10000000 < numeros.txt >> salFibonacci_sinHilos.txt
 
+# Parámetros a pasar al programa SIN HILOS
+n2="$2"
+n3="$3"
+n4="$4"
+output_file="salFibonacci_sinHilos.txt"
+
+# Ejecuta el programa con los parámetros y los archivos de texto
+./fibonacci_sinHilos "$n2" "$n3" "$n4" numeros.txt tamano.txt numero.txt > "$output_file"
