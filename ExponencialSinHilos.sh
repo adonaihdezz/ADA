c72aef96f8dcbe67e3df463cbf29aa73bb2aa557
adonaihdezz <119274699+adonaihdezz@users.noreply.github.com>
2023-05-05 18:18:01 -0600

    Update ExpoSinHilos.sh

--- ExponencialSinHilos.sh
+++ ExponencialSinHilos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gcc exponential.c tiempo.c -o exponential -w 
-./exponential 1000000 197 < 10millones.txt #>> expo_salida.txt
+./exponential 1000000 197 < 10millones.txt #>> expo_salida.txt 
 
