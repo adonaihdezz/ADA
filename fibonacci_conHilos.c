5cd40c8e3e53caa8f68f7f1f86cbea32653c3dca
IcyHavit <128344897+IcyHavit@users.noreply.github.com>
2023-05-05 20:36:49 -0600

    Update fibonacci_conHilos.c

--- fibonacci_conHilos.c
+++ fibonacci_conHilos.c
@@ -1,13 +1,11 @@
-
-
 /*
  * ANALISIS Y DISENO DE ALGORITMOS
  *
- *ALGORITMO HEAPSORT DE LA PRACTICA 1: IMPLEMENTACION Y ANALISIS DE EJECUCION DEL ALGORITMO FUBONACCI SIN HILOS.
+ *ALGORITMO HEAPSORT DE LA PRACTICA 1: IMPLEMENTACION Y ANALISIS DE EJECUCION DEL ALGORITMO FUBONACCI.
  * -------------------
  * EQUIPO: LOS BELICOS
  * 
- * VERSION DE CODIGO: 12.0
+ * VERSION DE CODIGO: 14.0
  * 
  *FECHA: 15/04/2023
  *
@@ -23,86 +21,113 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "recursos/tiempo.h"
+#include <pthread.h>
 
+int NumThreads;			//N�mero de threads
+int N,*A,num,n,ind=0;
+
+
+void* saludar(void* id);
 int min(int x, int y);
 int fibMonaccianSearch(int *a, int x, int n);
+void* procesar(void* id);
 
 
 int main(int arg, char* argv[])
 {
-	int num;
-	int *numeros_a_buscar,*A, *tamano; //Arreglo de numeros a buscar y arreglo de tama�os
+	//aqui si dejo este arreglo
+	long int numeros_a_buscar[] = {322486, 14700764, 3128036, 6337399, 61396, 10393545, 2147445644, 1295390003, 450057883, 187645041, 1980098116, 152503, 5000, 1493283650, 214826, 18433949527, 1360839354, 2109248666, 2147470852, 0};
+    int num_numeros_a_buscar = sizeof(numeros_a_buscar) / sizeof(numeros_a_buscar[0]);
+
 	int i,j,k;
 	double utime0, stime0, wtime0,utime1, stime1, wtime1; //Variables para medici�n de tiempos
 	double utime02, stime02, wtime02,utime12, stime12, wtime12;
-
-	//******************************************************************
-	//variables para leer archivos
-    int n, n2,n3,ind;
-    FILE *fp1, *fp2,*fp3;
-	//******************************************************************
-
-	//leyendo argumentos y generando espacio para los arreglos
 	
-    n = atoi(argv[1]);
-    A = (int*) malloc(n * sizeof(int));
 
-   	n2  = atoi(argv[2]);
-    tamano = (int*) malloc(n2 * sizeof(int));
-
-    n3 = atoi(argv[3]);
-    numeros_a_buscar = (int*) malloc(n3 * sizeof(int));
-
-
-	//abriendo archivos
-
-    fp1 = fopen(argv[4], "r");
-    fp2 = fopen(argv[5], "r");
-	fp3 = fopen(argv[6], "r");
-
-
-	//revisando que los archivos se hayan abierto correctamente
-    if (fp1 == NULL || fp2 == NULL || fp3 == NULL) {
-        printf("Error al abrir los archivos de texto.\n");
-        exit(1);
-    }
-
-	//leyendo los archivos y guardando los datos en los arreglos
-    for (i = 0; i < n; i++) {
-        fscanf(fp1, "%d", &A[i]);
-    }
-
-	for (i = 0; i < n2; i++) {
-        fscanf(fp2, "%d", &tamano[i]);
-    }
-
-    for (i = 0; i < n3; i++) {
-        fscanf(fp3, "%d", &numeros_a_buscar[i]);
-    }
-
-
-    //cerrando archivos
-    fclose(fp1);
-    fclose(fp2);
-	fclose(fp3);
-	//******************************************************************
-
-	printf("\n---------------------Implementacion sin hilos---------------------\n\n");
-
-	// en n2 se guardan los tama�os de los arreglos, por eso se usa para el ciclo
-	for( i = 0; i < n2; i++)
+	pthread_t *thread;
+//***************************************************************************************************************************
+// Verificar par�metros y llenar el arreglo
+//***************************************************************************************************************************	
+	if (arg<2) 
 	{
-		printf("\n\npara n: %d\n\n",tamano[i]);
-		//en n3 se guardan los numeros a buscar, por eso se usa para el ciclo
-		for (j = 0; j < n3 ;j++) {
+		printf("Indique el n�mero de threads - \tEjemplo: [user@equipo]$ %s 4\n\n",argv[0]);
+		exit(-1);
+	}
+	
+	NumThreads=atoi(argv[1]);
+	thread = malloc(NumThreads*sizeof(pthread_t));
+    
+	if(arg != 3)
+	{
+		printf("\n\n Para compilar se necesita tamanio de arreglo");
+		exit(1);
+	}
+	
+	//aqui lo deje igual como lo haciamos antes
+    n= atoi(argv[2]);
+    A = (int*) malloc(n * sizeof(int));
+    
+    
+    for (i = 0; i < n; i++) 
+    scanf("%d", &A[i]);
+
+	printf("\n--------------------------Implementacion con hilos----------------------------- \n\n");
+//***************************************************************************************************************************
+
+    
+//***************************************************************************************************************************
+// Saludar desde cada hilo "saludar"
+//***************************************************************************************************************************
+	//Crear los threads con el comportamiento "segmentar"
+	for (i=1; i<NumThreads; i++) 
+	{
+		if (pthread_create (&thread[i], NULL, saludar,(void*)i) != 0 ) 
+		{
+			perror("El thread no  pudo crearse");
+			exit(-1);
+		}
+	}
+	
+	//El main ejecuta el thread 0
+	saludar(0);
+	
+	
+	//Esperar a que terminen los threads (Saludar)
+	for (i=1; i<NumThreads; i++) pthread_join (thread[i], NULL);
+				
+	
+	printf("\n");
+				
+	//***************************************************************************************************************************
+	//2 Procesar desde cada hilo "procesar"
+	//***************************************************************************************************************************
+	
+
+//***************************************************************************************************************************
+// Buscar cada n�mero en el arreglo
+//***************************************************************************************************************************
+	printf("\n\npara n: %d con %d hilos\n\n",n, NumThreads);
+
+
+	//esto ya lo explicamos en clase
+	for (j = 0; j < num_numeros_a_buscar ;j++) {
 
 			num= numeros_a_buscar[j];
 
-			printf("\n\n-----> Buscando %d\n", num);
+			printf("\n\n------> Buscando %d\n", num);
 
+
+			for (int k = 1; k < NumThreads; k++) 
+			{
+				if (pthread_create(&thread[k], NULL, procesar, (void*)k) != 0 ) 
+				{
+					perror("El thread no pudo crearse");
+					exit(-1);
+				}
+			}
 
 			uswtime(&utime0, &stime0, &wtime0);
-			ind = fibMonaccianSearch(A, num, n);
+			procesar(0);
 			uswtime(&utime1, &stime1, &wtime1);
 
 			utime02+=utime0;
@@ -119,8 +144,10 @@ int main(int arg, char* argv[])
 			stime1=0;
 			wtime1=0;
 				
+			//Esperar a que terminen los threads (Saludar)
+			for (int k = 1; k < NumThreads; k++) pthread_join(thread[k], NULL);
 
-			//Imprimir el numero si se encontro o no, yo no lo imprimo porque llena el archivo de texto de datos
+			//Imprimir el numero si se encontro o no
 
 			/*
 			if(ind >= 0)
@@ -129,38 +156,35 @@ int main(int arg, char* argv[])
 				printf("\n\nel numero %d no se encuentra en el arreglo\n\n ",num);
 			*/
 	
-		}	
-
-		printf("\n\n");
-
-		printf("\nTiempo de ejecucion del programa\n");
-		printf("real (Tiempo total):  %.10f s\n",  (wtime12 - wtime02)/20);
-		printf("user (Tiempo de procesamiento en CPU): %.10f s\n",  (utime12 - utime02)/20);
-		printf("sys (Tiempo en acciónes de E/S):  %.10f s\n",  (stime12 - stime02)/20);
-		printf("CPU/Wall:   %.10f %% \n",100.0 * ((utime12 - utime02 + stime12 - stime02) / (wtime12 -wtime02))/20);
-		printf("\n");
-		
-		//Mostrar los tiempos en formato exponecial
-		printf("\nFormato exponencial\n");
-		printf("real (Tiempo total):  %.10e s\n",  (wtime12 - wtime02)/20);
-		printf("user (Tiempo de procesamiento en CPU): %.10e s\n",  (utime12 - utime02)/20);
-		printf("sys (Tiempo en acciónes de E/S):  %.10e s\n",  (stime12 - stime02)/20);
-		printf("CPU/Wall:   %.10f %% \n",100.0 * ((utime12 - utime02 + stime12 - stime02) / (wtime12 - wtime02))/20);
-		printf("\n");
-		printf("\n\n**************************************\n\n");
-
 	}
+
+
+	//***************************************************************************************************************************
+	// Calcular los tiempos de ejecuci�n
+	//***************************************************************************************************************************
+	printf("\n\n");
+
+	printf("\nTiempo de ejecucion del programa\n");
+	printf("real (Tiempo total):  %.10f s\n",  (wtime12 - wtime02)/20);
+	printf("user (Tiempo de procesamiento en CPU): %.10f s\n",  (utime12 - utime02)/20);
+	printf("sys (Tiempo en acciónes de E/S):  %.10f s\n",  (stime12 - stime02)/20);
+	printf("CPU/Wall:   %.10f %% \n",100.0 * ((utime12 - utime02 + stime12 - stime02) / (wtime12 -wtime02))/20);
+	printf("\n");
 	
+	//Mostrar los tiempos en formato exponecial
+	printf("\nFormato exponencial\n");
+	printf("real (Tiempo total):  %.10e s\n",  (wtime12 - wtime02)/20);
+	printf("user (Tiempo de procesamiento en CPU): %.10e s\n",  (utime12 - utime02)/20);
+	printf("sys (Tiempo en acciónes de E/S):  %.10e s\n",  (stime12 - stime02)/20);
+	printf("CPU/Wall:   %.10f %% \n",100.0 * ((utime12 - utime02 + stime12 - stime02) / (wtime12 - wtime02))/20);
+	printf("\n");
+	printf("\n\n**************************************\n\n");
 
-
-
-		
+	
 	free(A);
 	return 0;
 
 }
-
-
 
 /*
 DESCRIPCION:La funcion min encuentra el minimo entre dos numeros. 
@@ -235,4 +259,40 @@ int fibMonaccianSearch(int *a, int x, int n)
 
 	/*element not found. return -1 */
 	return -1;
+}
+
+
+/*
+DESCRIPCION:La funcion procesar es la funcion que ejecutan los threads.
+PARAMETRO id: Identificador del thread.
+*/
+void* procesar(void* id)
+{	
+	int n_thread=(int)id;
+	int inicio,fin,i;
+
+	//Revisar la parte de los datos a procesar	
+	inicio=(n_thread*n)/NumThreads;
+	if(n_thread==NumThreads-1)	
+		fin=n;
+	else
+		fin=((n_thread+1)*n)/NumThreads-1;
+	
+
+	printf("\nHola desde procesar\tSoy el thread %d\tInicio %d\tTermino %d",n_thread,inicio,fin);
+	
+	ind = fibMonaccianSearch(A, num, n);
+
+	printf("\nBye desde procesar\tSoy el thread %d\tHe terminado",n_thread);
+
+}
+
+/*
+DESCRIPCION:La funcion saludar es la funcion que ejecutan los threads.
+PARAMETRO id: Identificador del thread.
+*/
+
+void* saludar(void* id)
+{
+	printf("\nHola desde Saludar\tSoy el thread %d", (int)id);
 }
