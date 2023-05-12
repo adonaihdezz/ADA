cd72d724d3da2e847ec60300b5758f67c664f51a
IcyHavit <128344897+IcyHavit@users.noreply.github.com>
2023-05-12 00:04:12 -0600

    Update huffman.c 3.0

--- Huffman/huffman.c
+++ Huffman/huffman.c
@@ -1,13 +1,18 @@
-/*ESTA PEQUEÑA PARTE DEL CODIGO HACE LA LECTURA DEL ARCHIVO Y CON UN BUCLE WHILE
-*DETECTA LOS BYTES QUE TIENE ESTE, DONDE LOS GUARDA EN UN ARREGLO DE CHARS Y CON ESE 
-*MISMO ARREGLO DE CHARS GUARDA EN OTRO ARREGLO LAS FRECUENCIA DE LOS BYTES
-*/
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX_BYTES 256 // número máximo de bytes diferentes (0-255)
+
+
+struct NodoHuffman {
+    unsigned char byte;
+    int frecuencia;
+    struct NodoHuffman *izquierdo;
+    struct NodoHuffman *derecho;
+};
+
+// Ordena los nodos por frecuencia de menor a mayor
+void ordenar_nodos(struct NodoHuffman *nodos, int tamano);
 
 int main() {
     char *filename = "archivo.bin"; // nombre del archivo a leer
@@ -21,14 +26,14 @@ int main() {
     char byte;
     long size = 0;
     char *bytes = NULL;
-    int frequencies[MAX_BYTES] = {0}; // arreglo para almacenar las frecuencias de cada byte
+    int frecuencias[MAX_BYTES] = {0}; // arreglo para almacenar las frecuencias de cada byte
 
     // obtener los bytes del archivo y las frecuencias de cada byte
     while ((byte = fgetc(file)) != EOF) {
         size++;
         bytes = (char *) realloc(bytes, size); // asignar más memoria al arreglo
         bytes[size - 1] = byte;
-        frequencies[(unsigned char) byte]++;
+        frecuencias[(unsigned char) byte]++;
     }
 
     fclose(file); // cerrar el archivo
@@ -39,15 +44,48 @@ int main() {
     }
     printf("\n");
 
+    FILE *freq_file = fopen("frecuencias.txt", "w"); // abrir el archivo para escribir las frecuencias
+
     // imprimir las frecuencias de cada byte
     for (int i = 0; i < MAX_BYTES; i++) {
-        if (frequencies[i] > 0) {
-            printf("%02X: %d\n", i, frequencies[i]);
+        if (frecuencias[i] > 0) {
+            printf("%d: %d\n", i, frecuencias[i]);
         }
     }
-
+        
+    fclose(freq_file); // cerrar el archivo de frecuencias
+    
+    // crear un arreglo de nodos para cada byte en el archivo
+    struct NodoHuffman *nodos[MAX_BYTES];
+    for (int i = 0; i < MAX_BYTES; i++) {
+        if (frecuencias[i] > 0) {
+            nodos[i] = (struct NodoHuffman*) malloc(sizeof(struct NodoHuffman));
+            nodos[i]->byte = i;
+            nodos[i]->frecuencia = frecuencias[i];
+            nodos[i]->izquierdo = NULL;
+            nodos[i]->derecho = NULL;
+        } else {
+            nodos[i] = NULL;
+        }
+    }
+    
     free(bytes); // liberar la memoria asignada para el arreglo
 
     return 0;
 }
 
+// Ordena los nodos por frecuencia de menor a mayor
+void ordenar_nodos(struct NodoHuffman *nodos, int tamano) {
+    int i, j;
+    struct NodoHuffman temp;
+    
+    for (i = 0; i < tamano - 1; i++) {
+        for (j = 0; j < tamano - i - 1; j++) {
+            if (nodos[j].frecuencia > nodos[j+1].frecuencia) {
+                temp = nodos[j];
+                nodos[j] = nodos[j+1];
+                nodos[j+1] = temp;
+            }
+        }
+    }
+}
