ba3135c0b6c67863ddcfce63e71cd481b7344c97
adonaihdezz <119274699+adonaihdezz@users.noreply.github.com>
2023-05-15 19:27:56 -0600

    Update linealsinhilos.c

--- linealsinhilos.c
+++ linealsinhilos.c
@@ -1,3 +1,15 @@
+/*
+Analisis y diseno de algoritmos 
+
+Equipo: Los belicos 
+
+Version del codigo: 7 Lineal sin hilos 
+
+Fecha: 05/05/2023
+
+Realizo: Jesus Adonai Perez Hernandez
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include "tiempo.h"
@@ -10,7 +22,7 @@ int lineal(int *a, int num, int n);
 int main(int arg, char *argv[])
 {
 	int i;
-	int numeros_a_buscar[] = {162312,0,197,2096,2293};
+	int numeros_a_buscar[] = {322486, 14700764, 3128036, 6337399, 61396, 10393545, 2147445644, 1295390003, 450057883, 187645041, 1980098116, 152503, 5000, 1493283650, 214826, 18433949527, 1360839354, 2109248666, 2147470852, 0};
 	int num_numeros_a_buscar = sizeof(numeros_a_buscar) / sizeof(numeros_a_buscar[0]);
 	
 	double utime0, stime0, wtime0, utime1, stime1, wtime1;
@@ -25,6 +37,9 @@ int main(int arg, char *argv[])
 	{
 		scanf("%d",&A[i]);
 	}
+	
+	uswtime(&utime0, &stime0, &wtime0);
+	
 	for(i = 0; i < num_numeros_a_buscar; i++)
 	{
 	int posicion = lineal(A,numeros_a_buscar[i], n);
@@ -34,8 +49,25 @@ int main(int arg, char *argv[])
 	}else	{
 	printf("%ld no encontrado\n",numeros_a_buscar[i]);
 		}
-	}
+			utime02+=utime0;
+			stime02+=stime0;
+			wtime02+=wtime0;
+			utime12+=utime1;
+			stime12+=stime1;
+			wtime12+=wtime1;
+
+			utime0=0;
+			stime0=0;
+			wtime0=0;
+			utime1=0;
+			stime1=0;
+			wtime1=0;	
+		
 	
+	uswtime(&utime1, &stime1, &wtime1);
+	
+				
+
 	printf("\n\n");
 
 	printf("\nTiempo de ejecucion del programa\n");
@@ -54,7 +86,7 @@ int main(int arg, char *argv[])
 	printf("\n");
 	printf("\n\n**************************************\n\n");
 
-	
+	}
 	free(A);
 	
 	return 0;
