e857650bf1106848b5e54c48ee55f5a19e3ad8fe
adonaihdezz <119274699+adonaihdezz@users.noreply.github.com>
2023-05-16 20:36:04 -0600

    Delete linealsinhilos.sh

--- linealsinhilos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-gcc linealsinhilos.c tiempo.c -o linealsinhilos -w
-
-./linealsinhilos 10000000 < 10millones.txt > SalidaLinealSinHilos.txt
-#Cambian tamano del arreglo 
-# ./linealsinhilos.sh  
