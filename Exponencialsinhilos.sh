2b46f37afea6682e634b913c4004911c99283bb7
adonaihdezz <119274699+adonaihdezz@users.noreply.github.com>
2023-05-16 20:36:59 -0600

    Delete Exponencialsinhilos.sh

--- Exponencialsinhilos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gcc Exponencialsinhilos.c tiempo.c -o Exponentialsinhilos -w 
-./Exponentialsinhilos 10000000 < 10millones.txt > SalidaExponencialsinhilos.txt 
-# Cambia el tamano del arreglo (n)
-# ./Exponencialsinhilos.sh 
